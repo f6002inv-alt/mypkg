0a8f66c02073cf6eb46f2ffabf7d80560a1a03fe
Riku Terashima <f6002inv@sit.ac.jp>
2026-07-16 12:19:11 +0900

    Fix

--- test/test.bash
+++ test/test.bash
@@ -16,6 +16,9 @@ colcon build --symlink-install
 source install/setup.bash
 source install/local_setup.bash
 
+export PYTHONPATH=$PYTHONPATH:$dir/ros2_ws/install/person_msgs/local/lib/python3.10/dist-packages
+export PYTHONPATH=$PYTHONPATH:$dir/ros2_ws/install/person_msgs/lib/python3.10/site-packages
+
 timeout 15 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 cat /tmp/mypkg.log
 cat /tmp/mypkg.log | grep 'Listen:'
