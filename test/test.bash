5ede221e5db76598e2b0c43747a7d3d197829835
Riku Terashima <f6002inv@sit.ac.jp>
2026-07-17 10:55:47 +0900

    Add a test

--- test/test.bash
+++ test/test.bash
@@ -4,21 +4,11 @@ dir=~
 [ "$1" != "" ] && dir="$1"
 
 cd $dir/ros2_ws
+colcon build
 
-source /opt/ros/humble/setup.bash
+source $dir/.bashrc
 
-colcon build --packages-select person_msgs --symlink-install
+timeout 10 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
 
-source install/setup.bash
-
-colcon build --symlink-install
-
-source install/setup.bash
-source install/local_setup.bash
-
-export PYTHONPATH=$PYTHONPATH:$dir/ros2_ws/install/person_msgs/local/lib/python3.10/dist-packages
-export PYTHONPATH=$PYTHONPATH:$dir/ros2_ws/install/person_msgs/lib/python3.10/site-packages
-
-timeout 15 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log
-cat /tmp/mypkg.log
-cat /tmp/mypkg.log | grep 'Listen:'
+cat /tmp/mypkg.log |
+grep 'Listen: 10'
