655b9aab32f1095fa983b737f1b9fb057299ad3a
Riku Terashima <f6002inv@sit.ac.jp>
2026-07-22 12:53:20 +0900

    Fix

---
+++ build/mypkg/build/lib/mypkg/talker.py
@@ -0,0 +1,18 @@
+import rclpy
+from rclpy.node import Node
+from person_msgs.srv import Query
+
+
+def cb(request, response):
+    if request.name == "j2094":
+        response.age = 22
+    else:
+        response.age = 255
+    return response
+
+rclpy.init()
+node = Node("talker")
+srv = node.create_service(Query, "query", cb)
+rclpy.spin(node)
+
+
